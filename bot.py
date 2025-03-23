ef06d5fc186f72cfef2d8d839ad895fa9ab10981
whitebangul <whitebangul0819@gmail.com>
2025-03-23 00:58:12 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,13 +11,15 @@ TOKEN = os.getenv("TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
+class MyBot(commands.Bot):
+    async def setup_hook(self):
+       await self.load_extension("cogs.dice")
+
 bot = commands.Bot(command_prefix="-", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
     await bot.change_presence(activity=discord.Game(name=""))
-
-bot.load_extension("cogs.dice")
 
 bot.run(TOKEN)
